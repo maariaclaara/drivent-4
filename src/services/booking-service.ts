f61dbde34f67558594b98c9c90a6dcb89b6e7194
Maria Clara <129561798+maariaclaara@users.noreply.github.com>
2023-11-26 23:18:28 -0300

    update

---
+++ src/services/booking-service.ts
@@ -0,0 +1,68 @@
+import { cannotBookingError } from '@/errors/cannot-booking-error';
+import { notFoundError } from '@/errors';
+import { enrollmentRepository } from '@/repositories/enrollments-repository';
+import { ticketsRepository } from '@/repositories/tickets-repository';
+import { roomRepository } from '@/repositories/room-repository';
+import { bookingRepository } from '@/repositories/booking-repository';
+
+async function checkEnrollmentTicket(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw cannotBookingError();
+  }
+
+  const ticket = await ticketsRepository.findTicketByEnrollmentId(enrollment.id);
+  if (!ticket || ticket.status === 'RESERVED' || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw cannotBookingError();
+  }
+}
+
+async function checkValidBooking(roomId: number) {
+  const room = await roomRepository.findById(roomId);
+  const bookings = await bookingRepository.findByRoomId(roomId);
+  if (!room) {
+    throw notFoundError();
+  }
+  if (room.capacity <= bookings.length) {
+    throw cannotBookingError();
+  }
+}
+
+async function getBooking(userId: number) {
+  const booking = await bookingRepository.findByUserId(userId);
+  if (!booking) {
+    throw notFoundError();
+  }
+  return booking;
+}
+
+async function bookingRoomById(userId: number, roomId: number) {
+  await checkEnrollmentTicket(userId);
+  await checkValidBooking(roomId);
+  return bookingRepository.create({ userId, roomId });
+}
+
+async function updateBookingRoomById(userId: number, roomId: number) {
+  await checkValidBooking(roomId);
+  const booking = await bookingRepository.findByUserId(userId);
+
+  if (!booking || booking.userId !== userId) {
+    throw cannotBookingError();
+  }
+
+  return bookingRepository.updateBooking({
+    id: booking.id,
+    userId,
+    roomId,
+  });
+}
+
+const bookingService = {
+  bookingRoomById,
+  getBooking,
+  checkEnrollmentTicket,
+  checkValidBooking,
+  updateBookingRoomById,
+};
+
+export default bookingService;
